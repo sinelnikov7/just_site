eb8e14235fea7e15b055aba182dbc350581e694b
unknown <sinelnikov@gmail.com>
2022-05-05 20:12:33 +0300

    12121

--- market_30/main/views.py
+++ market_30/main/views.py
@@ -6,6 +6,24 @@ from .models import Category, Product
 def main_page(request):
     categories = Category.objects.all()
     products = Product.objects.all()
+    print(request.POST)
+    if request.method == 'POST':
+        product = Product.objects.filter(title=request.POST.get('find'))
+        #disc = Product.objects.get(discription = product)
+        print(product)
+        if product:
+            context = {
+                'product': product,
+                'categories': categories,
+            }
+            return render(request, 'main_page.html', context)
+
+        else:
+            context = {
+                'error': 'Поиск не дал результатов',
+                'categories': categories,
+            }
+            return render(request, 'main_page.html', context)
     context = {
 
         'categories': categories,
